fa417225d70e9d53f82e562f0c9ea19d2dbb3f89
Michael McEniry [UAH] <mcenirm@uah.edu>
2017-03-14 14:04:10 -0500

    Test: Can per_stirpes.main launch a process?

--- test_per_stirpes.py
+++ test_per_stirpes.py
@@ -4,5 +4,6 @@ import per_stirpes
 
 
 class PerStirpesTests(unittest.TestCase):
-    def testPlaceholder(self):
-        self.assertTrue(True)
+    def test_main_can_launch_a_process(self):
+        returncode = per_stirpes.main(['true'])
+        self.assertEqual(0, returncode, 'true command should have exit status 0')
