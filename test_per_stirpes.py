8e7e6eb6e4b4023432f2f7210ddbf75d5e799a01
Michael McEniry [UAH] <mcenirm@uah.edu>
2017-03-14 14:07:58 -0500

    Test: Can per_stirpes.main launch a process and return the proper exit status?

--- test_per_stirpes.py
+++ test_per_stirpes.py
@@ -7,3 +7,5 @@ class PerStirpesTests(unittest.TestCase):
     def test_main_can_launch_a_process(self):
         returncode = per_stirpes.main(['true'])
         self.assertEqual(0, returncode, 'true command should have exit status 0')
+        returncode = per_stirpes.main(['false'])
+        self.assertNotEqual(0, returncode, 'false command should not have exit status 0')
